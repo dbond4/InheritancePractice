51b3e7b33e2e2d5e59783791b1cbdea541af57a4
dbond4@my.wctc.edu <>
2013-01-28 15:37:05 -0600

    added classes employee, hourlyemployee, salariedemployee, and salaryplusbonousemployee

---
+++ src/myinterface/Employee.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package myinterface;
+
+/**
+ *
+ * @author F03 H4MM3R
+ */
+public class Employee {
+    
+}
